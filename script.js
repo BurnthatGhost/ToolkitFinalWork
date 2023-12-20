617f714a35a8c8cae6e776dc56345490615257dd
焚魔 <152372007+BurnthatGhost@users.noreply.github.com>
2023-12-21 03:59:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let ws = new WebSocket("wss://websocket-jfe7.onrender.com/");
+
+let controllTD = document.querySelector('.controllTD') ;
+controllTD.addEventListener('input', (event) => {
+  ws.send(JSON.stringify({ 'slider1': controllTD.value / 100 }));
+}, false);
+
+ws.addEventListener('open', (event) => {
+  console.log('Socket connection open!');
+  // alert('Successfully connected to socket server 🎉');
+  ws.send('pong');
+});
+
+ws.addEventListener('error', (error) => {
+    console.error('Error in the connection', error);
+    alert('error connecting socket server', error);
+});
+
+ws.addEventListener('close', (event) => {
+    console.log('Socket connection closed');
+    alert('closing socket server');
+});
